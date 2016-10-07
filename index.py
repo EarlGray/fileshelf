ffae4082d4d985c7098d3f00eb479532b47850e6
Dmytro S <dmytrish@gmail.com>
2016-10-08 00:39:34 +0200

    directory icons, sorting

--- index.py
+++ index.py
@@ -1,20 +1,30 @@
 import os
+import stat
+
 import flask
 from flask import Flask
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
+
 storage_dir = None
+static_dir = None
 my_url = '/my'
+
+
+def url_join(*args):
+    return '/'.join([arg.rstrip('/') for arg in args if len(arg)])
 
 
 def path_prefixes(path):
     url = my_url
     res = []
     for d in path.split('/'):
-        url = url + '/' + d
+        url = url_join(url, d)
         res.append((d, url))
     return res
+
+app = Flask(__name__)
+app.debug = False
 
 
 @app.route('/')
@@ -23,7 +33,7 @@ def homepage_handler():
 
 
 @app.route(my_url, defaults={'path': ''}, methods=['GET', 'POST'])
-@app.route(my_url + '/<path:path>', methods=['GET', 'POST'])
+@app.route(url_join(my_url, '<path:path>'), methods=['GET', 'POST'])
 def path_handler(path):
     req = flask.request
 
@@ -53,16 +63,19 @@ def path_handler(path):
     if not os.path.isdir(dpath):
         return flask.send_file(dpath)
 
-    lsdir = {}
+    lsdir = []
     try:
-        for entry in os.listdir(dpath):
-            fname = entry.decode('utf-8')
-            lsdir[fname] = {
-                'href': os.path.join(my_url, path, fname)
-            }
+        for fname in os.listdir(dpath):
+            st = os.lstat(os.path.join(dpath, fname))
+            lsdir.append({
+                'name': fname,
+                'href': url_join(my_url, path, fname),
+                'size': st.st_size,
+                'isdir': stat.S_ISDIR(st.st_mode)
+            })
         templvars = {
             'path': path,
-            'lsdir': lsdir,
+            'lsdir': sorted(lsdir, key=lambda d: (not d['isdir'], d['name'])),
             'path_prefixes': path_prefixes(path)
         }
         return flask.render_template('dir.htm', **templvars)
@@ -70,14 +83,35 @@ def path_handler(path):
         return flask.render_template('404.htm', path=path), 404
 
 
+@app.route('/res/<path:path>')
+def static_handler(path):
+    fname = secure_filename(path)
+    fname = os.path.join(static_dir, fname)
+    if not os.path.exists(fname):
+        return flask.render_template('404.htm', path=path), 404
+
+    print 'Serving %s' % fname
+    return flask.send_file(fname)
+
+
+def error_handler(code, e=None, path=None):
+    return flask.render_template('%d.htm' % code, e=e, path=path), code
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return flask.render_template('404.htm'), 404
+
+
 @app.errorhandler(500)
-def error_handler(e):
+def internal_error(e):
     return flask.render_template('500.htm', e=e), 500
 
 if __name__ == '__main__':
-    app.debug = True
     storage_dir = os.path.join(os.getcwd(), 'storage')
+    static_dir = os.path.join(os.getcwd(), 'static')
 
     for k, v in app.config.iteritems():
         print '%s\t: %s' % (k, v)
-    app.run()
+
+    app.run(host='0.0.0.0', port=5000)
