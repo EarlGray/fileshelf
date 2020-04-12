f9cfdf53c8d0294a417bafb3b1aff3b021e93403
Dmytro S <me@dmytrish.net>
2020-04-12 13:54:25 +0100

    Depedencies updates.

--- fileshelf.sh
+++ fileshelf.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-export APPDIR="`dirname $0`"
+APPDIR="$(dirname "$0")"
+export APPDIR
 
 if which virtualenv >/dev/null ; then
-  if ! test -d $APPDIR/v3nv ; then
-    virtualenv -p python3 $APPDIR/v3nv
-    $APPDIR/v3nv/bin/pip install -r $APPDIR/requirements.txt
+  if ! test -d "$APPDIR/venv" ; then
+    virtualenv -p python3 "$APPDIR/venv"
+    "$APPDIR/venv/bin/pip" install -r "$APPDIR/requirements.txt"
   fi
-  . $APPDIR/v3nv/bin/activate
+  . "$APPDIR/venv/bin/activate"
 else
   echo "WARN: virtualenv not found, trying to use system libraries..." >&2
 fi
 
-exec $APPDIR/index.py "$@"
+exec "$APPDIR/index.py" "$@"
